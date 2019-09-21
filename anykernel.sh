ed1eb9eb478128ce05894923cdba44e74298595a
freak07 <michalechner92@googlemail.com>
2019-09-21 15:23:26 +0200

    kirin: here I am thanks to @osm0sis for a slight kick in the right direction

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,23 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
+kernel.string=Kirisakura-Kernel for Asus Zenfone 6 aka Kirin by freak07 @ xda-developers
+do.devicecheck=0
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=
+device.name2=
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
 ramdisk_compression=auto;
 
 
@@ -34,29 +34,77 @@ ramdisk_compression=auto;
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+## begin vendor changes
+mount -o rw,remount -t auto /vendor >/dev/null;
+
+cp -rf /tmp/anykernel/patch/init.qcom.post_boot.sh /vendor/bin/init.qcom.post_boot.sh;
+set_perm 0 0 0755 /vendor/bin/init.qcom.post_boot.sh;
+
+cp -rf /tmp/anykernel/patch/msm_irqbalance.conf /vendor/etc/msm_irqbalance.conf;
+set_perm 0 0 0644 /vendor/etc/msm_irqbalance.conf;
+
 
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+# Add skip_override parameter to cmdline so user doesn't have to reflash Magisk
+if [ -d $ramdisk/.backup ]; then
+  ui_print " "; ui_print "Magisk detected! Patching cmdline so reflashing Magisk is not necessary...";
+  patch_cmdline "skip_override" "skip_override";
+else
+  patch_cmdline "skip_override" "";
+fi;
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
+if mountpoint -q /data; then
+  # Optimize F2FS extension list (@arter97)
+  for list_path in $(find /sys/fs/f2fs* -name extension_list); do
+    hash="$(md5sum $list_path | sed 's/extenstion/extension/g' | cut -d' ' -f1)"
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+    # Skip update if our list is already active
+    if [[ $hash == "43df40d20dcb96aa7e8af0e3d557d086" ]]; then
+      echo "Extension list up-to-date: $list_path"
+      continue
+    fi
+
+    ui_print "  • Optimizing F2FS extension list"
+    echo "Updating extension list: $list_path"
+
+    echo "Clearing extension list"
+
+    hot_count="$(grep -n 'hot file extens' $list_path | cut -d':' -f1)"
+    list_len="$(cat $list_path | wc -l)"
+    cold_count="$((list_len - hot_count))"
+
+    cold_list="$(head -n$((hot_count - 1)) $list_path | grep -v ':')"
+    hot_list="$(tail -n$cold_count $list_path)"
+
+    for ext in $cold_list; do
+      [ ! -z $ext ] && echo "[c]!$ext" > $list_path
+    done
+
+    for ext in $hot_list; do
+      [ ! -z $ext ] && echo "[h]!$ext" > $list_path
+    done
+
+    echo "Writing new extension list"
+
+    for ext in $(cat $home/f2fs-cold.list | grep -v '#'); do
+      [ ! -z $ext ] && echo "[c]$ext" > $list_path
+    done
+
+    for ext in $(cat $home/f2fs-hot.list); do
+      [ ! -z $ext ] && echo "[h]$ext" > $list_path
+    done
+  done
+fi
 
 # end ramdisk changes
+    
+	ui_print " "; ui_print "Flashing Kirisakura-Kernel. Enjoy your device and have a nice day!";
+	ui_print " "; ui_print "Please make sure to always run the latest companion magisk module!";
+
 
 write_boot;
 ## end install
